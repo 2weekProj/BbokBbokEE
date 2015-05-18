3a8b68f1245042821ac1fcb32e3c91e1379d9f62
syu <cozy@mageoftheeast.com>
2015-05-18 22:25:18 +0900

    프로젝 파일 커밋

---
+++ source/ASRM Project/Assets/Script/AutoHideObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoHideObject : MonoBehaviour {
+
+	void Start ()
+	{
+		m_SpriteOut.enabled = false;
+	}
+	
+	void Update ()
+	{
+
+	}
+
+	void OnClick ()
+	{
+		if (a == true)
+		{
+			m_StartTime = Time.realtimeSinceStartup;
+			m_SpriteIn.enabled = false;
+			m_SpriteOut.enabled = true;
+			a = false;
+
+			if ( a == false)
+			{
+				StartCoroutine("Wait");
+			}
+		}
+	}
+
+	//------------------------------------------
+	public UISprite						m_SpriteIn = null;
+	public UISprite						m_SpriteOut = null;
+	public float						m_HideTimer = 1f;
+	//------------------------------------------
+	private bool						a = true;
+	private float						m_StartTime = 0f;
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(4);
+
+		float nowTime = Time.realtimeSinceStartup;
+		float deltaTime = nowTime - m_StartTime;
+
+		if (m_HideTimer <= deltaTime)
+		{
+			m_SpriteIn.enabled = true;
+			m_SpriteOut.enabled = false;
+			a = true;
+		}
+	}
+}
