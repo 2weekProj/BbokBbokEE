d92722069a6f0cea8f790682beca2bfc23455558
flowerotter <flowerotter@gmail.com>
2015-05-22 00:38:41 +0900

    Tab(2)
    버튼별 사운드 연결 작업 진행 및 테스트 중.

---
+++ source/ASRM Project/Assets/Script/MusciObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusciObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnClick ()
+	{
+		Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Ray2D ray = new Ray2D(touchPosition, Vector2.zero);
+		RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+		Debug.Log(hit.collider.tag);
+		
+		if(hit.collider.tag == "Music_01_01")
+		{
+			Debug.Log("2222");
+			audio.clip = SoundA;
+			audio.Stop();
+         	audio.Play();
+		}
+		if(hit.collider.tag == "Music_01_02")
+		{
+			Debug.Log("3333");
+			audio.clip = SoundA;
+         	audio.Stop();
+         	audio.Play();
+		}
+		if(hit.collider.tag == "Music_01_03")
+		{
+			Debug.Log("4444");
+		}
+	}
+	public AudioClip SoundA;
+}
