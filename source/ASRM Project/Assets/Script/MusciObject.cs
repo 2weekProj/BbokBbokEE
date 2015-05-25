4efea856aefd6675110b0c3963e24dba80106d00
flowerotter <flowerotter@gmail.com>
2015-05-25 21:52:46 +0900

    탭(2) 사운드 기본 작업중

--- source/ASRM Project/Assets/Script/MusciObject.cs
+++ source/ASRM Project/Assets/Script/MusciObject.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MusciObject : MonoBehaviour {
@@ -6,7 +6,7 @@ public class MusciObject : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-
+		_aa = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +22,65 @@ public class MusciObject : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 		Debug.Log(hit.collider.tag);
 		
+		if(hit.collider.tag != null)
+		{
+			//_aa.Stop();
+
+			switch(hit.collider.tag)
+			{
+				case "Music_01_01":	
+				_aa.Stop();
+				_aa.clip = Sample1;
+				_aa.Play();
+				break;
+
+				case "Music_01_02":
+				_aa.Stop();
+				_aa.clip = Sample2;
+				_aa.Play();
+				break;
+			}
+		}
+	}
+	public AudioClip Sample1;
+	public AudioClip Sample2;
+	
+	AudioSource _aa;
+}
+	
+/*
+	void OnClick ()
+	{
+		Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Ray2D ray = new Ray2D(touchPosition, Vector2.zero);
+		RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+		Debug.Log(hit.collider.tag);
+
+		if(hit.collider.tag != null)
+		{
+			
+			switch(hit.collider.tag)
+			{
+				case "Music_01_01":
+				audio.clip = SoundA;
+				audio.Play();
+				break;
+
+			 	case "Music_01_02":
+				audio.clip = SoundB;
+				audio.Play();
+				break;
+			}
+		}
+	}
+	public AudioClip SoundA;
+	public AudioClip SoundB;
+}
+*/
+
+
+
+/*
 		if(hit.collider.tag == "Music_01_01")
 		{
 			Debug.Log("2222");
@@ -41,5 +100,6 @@ public class MusciObject : MonoBehaviour {
 			Debug.Log("4444");
 		}
 	}
-	public AudioClip SoundA;
+	//AudioClip SoundB = null;
 }
+*/
