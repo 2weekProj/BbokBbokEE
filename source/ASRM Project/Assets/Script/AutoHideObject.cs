b8cb332b77b1fed0adb3ba2b85ad3e85bbe6875d
flowerotter <flowerotter@gmail.com>
2015-06-02 08:47:52 +0900

    출근전 급 커밋 랜덤 코드 수정중

--- source/ASRM Project/Assets/Script/AutoHideObject.cs
+++ source/ASRM Project/Assets/Script/AutoHideObject.cs
@@ -6,6 +6,7 @@ public class AutoHideObject : MonoBehaviour {
 	void Start ()
 	{
 		m_ButtonOut.enabled = false;
+		m_ButtonInEvent.enabled = false;
 		m_ButtonSound = gameObject.GetComponent<AudioSource>();
 	}
 	
@@ -21,55 +22,46 @@ public class AutoHideObject : MonoBehaviour {
 		switch(RandPoint)
 		{
 			case 0:
-			Debug.Log("0");
 			m_ButtonSound.clip = RandSound01;
 			m_ButtonSound.Play();
 			break;
 			
 			case 1:
-			Debug.Log("1");
 			m_ButtonSound.clip = RandSound02;
 			m_ButtonSound.Play();
 			break;
 			
 			case 2:
-			Debug.Log("2");
 			m_ButtonSound.clip = RandSound03;
 			m_ButtonSound.Play();
 			break;
 			
 			case 3:
-			Debug.Log("3");
 			m_ButtonSound.clip = RandSound04;
 			m_ButtonSound.Play();
 			break;
 			
 			case 4:
-			Debug.Log("4");
 			m_ButtonSound.clip = RandSound05;
 			m_ButtonSound.Play();
 			break;
 			
 			case 5:
-			Debug.Log("5");
 			m_ButtonSound.clip = RandSound06;
 			m_ButtonSound.Play();
 			break;
 			
 			case 6:
-			Debug.Log("6");
 			m_ButtonSound.clip = RandSound07;
 			m_ButtonSound.Play();
 			break;
 
 			case 7:
-			Debug.Log("7");
 			m_ButtonSound.clip = RandSound08;
 			m_ButtonSound.Play();
 			break;
 		
 			case 8:
-			Debug.Log("8");
 			m_ButtonSound.clip = RandSound09;
 			m_ButtonSound.Play();
 			break;
@@ -80,6 +72,7 @@ public class AutoHideObject : MonoBehaviour {
 			m_StartTime = Time.realtimeSinceStartup;
 			m_ButtonIn.enabled = false;
 			m_ButtonOut.enabled = true;
+			m_ButtonInEvent.enabled = false;
 			m_ButtonCheck = false;
 
 			if (m_ButtonCheck == false)
@@ -98,8 +91,17 @@ public class AutoHideObject : MonoBehaviour {
 
 		if (m_HideTimer <= deltaTime)
 		{
-			m_ButtonIn.enabled = true;
+			int RandButton = Random.Range(0,10);
 			m_ButtonOut.enabled = false;
+
+			if (RandButton == 0)
+			{
+				m_ButtonInEvent.enabled = true;
+			}
+			else
+			{
+				m_ButtonIn.enabled = true;
+			}
 			m_ButtonCheck = true;
 		}
 	}
@@ -109,6 +111,7 @@ public class AutoHideObject : MonoBehaviour {
 	//------------------------------------------
 	public UISprite						m_ButtonIn = null;
 	public UISprite						m_ButtonOut = null;
+	public UISprite						m_ButtonInEvent = null;
 	public int							m_ButtonWaitTime = 1; // 값이 1 이하면 안됨.
 	public AudioClip					RandSound01 = null;
 	public AudioClip					RandSound02 = null;
